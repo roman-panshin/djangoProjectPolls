70a4a7bf02a35cfb185cc00e6e5de157e9b76131
Roman Panshin <panshin13@gmail.com>
2023-07-01 14:16:37 +0300

    3-rd lesson completed added urls

--- djangoProjectPolls/settings.py
+++ djangoProjectPolls/settings.py
@@ -53,7 +53,7 @@ ROOT_URLCONF = 'djangoProjectPolls.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': [BASE_DIR / 'polls/templates']
         ,
         'APP_DIRS': True,
         'OPTIONS': {
--- djangoProjectPolls/urls.py
+++ djangoProjectPolls/urls.py
@@ -1,6 +1,7 @@
 """
 URL configuration for djangoProjectPolls project.
 
+
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
 Examples:
